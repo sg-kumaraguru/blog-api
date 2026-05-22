1629411e83c738547a58c22f1b22da45b2f571f6
sg-kumaraguru <sg.kumaraguru@gmail.com>
2026-05-22 23:09:01 +0530

    fix: post schema typo error

--- app/schemas/blogPostSchema.js
+++ app/schemas/blogPostSchema.js
@@ -4,12 +4,12 @@ export const createPostSchema = z.object({
   title: z.string().min(3),
   content: z.string().min(10),
   tags: z.array(z.string()).optional(),
-  status: z.enum(["draft", "published", "archived"]).optional(),
+  action: z.enum(["draft", "publish", "archived"]).optional(),
 });
 
 export const updatePostSchema = z.object({
   title: z.string().min(3).optional(),
   content: z.string().min(10).optional(),
   tags: z.array(z.string()).optional(),
-  status: z.enum(["draft", "published", "archived"]).optional(),
+  action: z.enum(["draft", "publish", "archived"]).optional(),
 });
